271f5331000b2a0da0e295d1f22ae51b73e1b27d
Ossi Myllymäki <omyllymaki@gmail.com>
2021-06-27 17:01:24 +0300

    Refactor solver

--- src/ransac_solver.py
+++ src/ransac_solver.py
@@ -20,50 +20,51 @@ class RansacSolver:
                  error_threshold: float,
                  n_sample_points: int,
                  max_trials: int = 100,
-                 n_inlier_threshold: int = None
+                 score_threshold: float = None
                  ):
         """
         @param model: Model that specifies fit and calculate_errors methods.
         @param error_threshold: Threshold value for errors; if error > threshold, it is considered as an outlier.
         @param n_sample_points: Number of points for random sample used to fit model candidate.
         @param max_trials: Max number of trials (iterations)
-        @param n_inlier_threshold: Threshold value for number inliers; stop iteration if number of inliers > threshold.
+        @param score_threshold: Threshold value for score; stop iteration if score > threshold.
         """
         self.model = model
         self.error_threshold = error_threshold
         self.max_trials = max_trials
         self.n_sample_points = n_sample_points
-        self.n_inlier_threshold = n_inlier_threshold
+        self.score_threshold = score_threshold
 
     def solve(self, data: BaseData) -> np.ndarray:
 
-        if self.n_inlier_threshold is None:
-            logger.debug(f"Number of inliers threshold is not specified. "
+        if self.score_threshold is None:
+            logger.debug(f"Score threshold is not specified. "
                          f"Set threshold to be equal to number of data points.")
-            self.n_inlier_threshold = len(data)
+            self.score_threshold = len(data)
 
-        max_n_inliers = 0
+        best_score = 0
         final_inlier_indices = None
 
         for trial_number in range(self.max_trials):
 
-            self._fit_with_random_sample(data)
+            sample = self.sample_data(data)
+            self.model.fit(sample)
 
-            inlier_indices = self._get_inliers(data)
-            n_inliers = len(inlier_indices)
-            logger.debug(f"Trial {trial_number}: number of inliers  {n_inliers}")
+            inlier_indices = self.get_inliers(data)
+            score = self.calculate_score(inlier_indices)
+            logger.debug(f"Trial {trial_number}: score  {score}")
 
-            # Update solution if number of inliers has grown
-            if n_inliers > max_n_inliers:
-                logger.info(f"Found better solution. Number of inliers {n_inliers}")
+            # Update solution if best so far
+            if score > best_score:
+                logger.info(f"Found better solution. Score {score}")
                 final_inlier_indices = inlier_indices
-                max_n_inliers = n_inliers
+                best_score = score
 
-                if max_n_inliers >= self.n_inlier_threshold:
-                    logger.info(f"Number of inliers exceed threshold level {self.n_inlier_threshold}.")
+                if best_score >= self.score_threshold:
+                    logger.info(f"Score {best_score} > threshold {self.score_threshold}. End iteration.")
                     break
 
-        logger.info(f"Iteration finished. Number of inliers: {max_n_inliers}")
+        logger.info(f"Iteration finished. Score: {best_score}")
 
         # Do final fitting with all the inliers
         logger.debug(f"Fit model with full inlier dataset.")
@@ -72,10 +73,12 @@ class RansacSolver:
 
         return final_inlier_indices
 
-    def _fit_with_random_sample(self, data):
-        sample = data.get_random_sample(self.n_sample_points)
-        self.model.fit(sample)
+    def sample_data(self, data: BaseData):
+        return data.get_random_sample(self.n_sample_points)
 
-    def _get_inliers(self, data):
+    def get_inliers(self, data: BaseData):
         errors = self.model.calculate_errors(data)
         return np.where(errors < self.error_threshold)[0]
+
+    def calculate_score(self, inlier_indices: np.ndarray) -> float:
+        return len(inlier_indices)
