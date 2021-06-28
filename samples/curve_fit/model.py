0483cc14cd9dddbb656b0552e2b7cfd6430643d4
Ossi Myllymäki <omyllymaki@gmail.com>
2021-06-28 17:56:43 +0300

    Handle scipy RuntimeError correctly in curve fit model

--- samples/curve_fit/model.py
+++ samples/curve_fit/model.py
@@ -12,20 +12,25 @@ class Model(BaseModel):
     Wrapper for scipy curve fit.
     """
 
-    def __init__(self, fit_function , **fit_method_kwargs):
+    def __init__(self, fit_function, **curve_fit_kwargs):
         self.fit_function = fit_function
-        self.curve_fit = partial(curve_fit, f=fit_function, **fit_method_kwargs)
+        self.curve_fit = partial(curve_fit, f=fit_function, **curve_fit_kwargs)
         self.parameters = None
 
     def fit(self, data: Data):
         try:
             self.parameters, _ = self.curve_fit(xdata=data.x, ydata=data.y)
-        except RuntimeError:
-            self.parameters = np.array([0, 0, 0])
+        except RuntimeError:  # curve_fit throws RuntimeError is solution is not found
+            self.parameters = None
 
     def estimate(self, data: Data):
-        return self.fit_function(data.x, *self.parameters)
+        if self.parameters is None:
+            return None
+        else:
+            return self.fit_function(data.x, *self.parameters)
 
     def calculate_errors(self, data: Data):
+        if self.parameters is None:
+            return np.inf * np.ones(data.y.shape)
         y_estimate = self.estimate(data)
         return np.abs(y_estimate - data.y)
